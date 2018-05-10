69b7d32bf318d232636709dceb6f9279c38e0d4a
Unknown <the_nightmare_doom@hotmail.com>
2018-05-09 22:45:05 -0300

    Auto stash before merge of "master" and "origin/master"

--- prueba.py
+++
@@ -1,7 +0,0 @@
-import urllib2
-from bs4 import BeautifulSoup
-
-f = urllib2.urlopen("https://www.zmart.cl/JuegosPS4")
-html = BeautifulSoup(f.read(), "html.parser")
-html = html.find("input",{"id":"curPage_32641"}).get("13")
-print html
