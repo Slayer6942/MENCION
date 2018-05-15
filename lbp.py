452e974738e1b4bbf5adcf981ebd20e6ca0d9157
apalominos <apalominos2014@alu.uct.cl>
2018-05-15 16:37:45 -0400

    LBP - Version 1

---
+++ lbp.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+def get_pixel(img, center, x, y):
+    new_value = 0
+    try:
+        if img[x][y] <= center:
+            new_value = 1
+    except:
+        pass
+    return new_value
+
+def lbp_calculated_pixel(img, x, y):
+    '''
+
+     64 | 128 |   1
+    ----------------
+     32 |   0 |   2
+    ----------------
+     16 |   8 |   4    
+
+    '''    
+    center = img[x][y]
+    val_ar = []
+    val_ar.append(get_pixel(img, center, x-1, y+1))     # top_right
+    val_ar.append(get_pixel(img, center, x, y+1))       # right
+    val_ar.append(get_pixel(img, center, x+1, y+1))     # bottom_right
+    val_ar.append(get_pixel(img, center, x+1, y))       # bottom
+    val_ar.append(get_pixel(img, center, x+1, y-1))     # bottom_left
+    val_ar.append(get_pixel(img, center, x, y-1))       # left
+    val_ar.append(get_pixel(img, center, x-1, y-1))     # top_left
+    val_ar.append(get_pixel(img, center, x-1, y))       # top
+    
+    power_val = [1, 2, 4, 8, 16, 32, 64, 128]
+    val = 0
+    for i in range(len(val_ar)):
+        val += val_ar[i] * power_val[i]
+
+    return val
+
+def main():
+    image_file = 'images/lenna.jpg'
+    img_bgr = cv2.imread(image_file)
+    height, width, channel = img_bgr.shape
+    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+    img_lbp = np.zeros((height, width,3), np.uint8)
+
+    for i in range(0, height):
+        for j in range(0, width):
+            img_lbp[i, j] = lbp_calculated_pixel(img_gray, i, j)
+
+if __name__ == '__main__':
+    main()
