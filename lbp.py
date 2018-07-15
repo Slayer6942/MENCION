080ae76c86e1293371450e9761d7dd83c5479992
Unknown <the_nightmare_doom@hotmail.com>
2018-07-15 17:35:59 -0400

    LBP v2.1

--- lbp.py
+++ lbp.py
@@ -3,6 +3,10 @@ from heapq import merge
 from skimage import exposure
 from skimage import feature
 import cv2
+import matplotlib.pyplot as plt
+from skimage.feature import hog
+from skimage import data, exposure
+
 
 def get_pixel(img, center, x, y):
     new_value = 0
@@ -34,7 +38,6 @@ def lbp_calculated_pixel(img, x, y):
 
 def analisis(imagen, archivo, tipo):
     img_bgr = cv2.imread(imagen)
-    HOG(img_bgr)
     height, width, channel = img_bgr.shape
     img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
     arry = [0] * (height * width)
@@ -46,52 +49,47 @@ def analisis(imagen, archivo, tipo):
     return arry
 
 def HOG(imagen):
-    (H, hogImage) = feature.hog(imagen, orientations=9, pixels_per_cell=(8, 8),cells_per_block=(2, 2), transform_sqrt=True, block_norm = "L1",visualise = True)
+    (H, hogImage) = feature.hog(imagen, orientations=9, pixels_per_cell=(8, 8),cells_per_block=(2, 2), transform_sqrt=True, block_norm = "L1",visualize = True)
     hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
     hogImage = hogImage.astype("uint8")
 
     cv2.imshow("HOG Image", hogImage)
 
-def escribir_datos(D, largo, tipo, archivo):
-    maximo = calcular_maximo(D)
-    print maximo, "maximo"
-    for i in range(largo-1):
-        if len(str(D[i]))> maximo:
-            print len(str(D[i]))
-        #print len(str(D[i])), "asasdfadfadfs"
-        diferencia = maximo - int(len(str(D[i])))
-        print len(str(D[i])), diferencia, len(str(D[i]))+ diferencia, len([0]*diferencia), len(D[i] + ([0]*diferencia))
-        prueba = list(merge(D[i],([0]*diferencia)))
-        print len(prueba)
-        #rint len(D[i] + ([0]*valor))
+def escribir_datos(D, largo, tipo, archivo, maximo):
+    maximo = maximo
+    for i in range(largo):
+        diferencia = maximo - len(D[i])
         agregar = D[i] + ([0]*diferencia)
-        #print len(str(D[i]))
         string1 = str(agregar)
         archivo.write(string1[1: -1] + "," + str(tipo) + "\n")
         #X= (X - mean(X))/std(X)
 
-def calcular_maximo(datos):
-    maximo = int(len(str(datos[0])))
-    for largo in datos:
-        if int(len(str(largo))) > maximo:
-            maximo = int(len(str(largo)))
+def calcular_maximo(datos1,datos2):
+    maximo = len(datos1[0])
+    for largo in datos1:
+        if len(largo) > maximo:
+            maximo = len(largo)
+    for largo in datos2:
+        if len(largo) > maximo:
+            maximo = len(largo)
     return maximo
 
 def main():
     datos = open("lbp.csv", "w")
-    positivas = len(os.listdir("entrenamiento/positivas"))
-    negativas = len(os.listdir("entrenamiento/negativas"))
-    positivas = 10
-    negativas = 10
-    datosP = [0] * positivas
-    datosN = [0] * negativas
+    positivas = len(os.listdir("entrenamiento/positivas"))-1
+    negativas = len(os.listdir("entrenamiento/negativas"))-1
+    #positivas = 10
+    #negativas = 10
+    datosP = [0] * (positivas)
+    datosN = [0] * (negativas)
 
-    for nu in range(positivas-1):
+    for nu in range(positivas):
         datosP[nu] = analisis("entrenamiento/positivas/" + str(nu) + ".jpg", datos, "P")
-    escribir_datos(datosP, positivas, 1, datos)
-    for nu in range(negativas-1):
+    for nu in range(negativas):
         datosN[nu] = analisis("entrenamiento/negativas/" + str(nu) + ".jpg", datos, "N")
-    escribir_datos(datosN, negativas, 0, datos)
+    maximo = calcular_maximo(datosP, datosN)
+    escribir_datos(datosP, positivas, 1, datos, maximo)
+    escribir_datos(datosN, negativas, 0, datos, maximo)
 
 if __name__ == '__main__':
     main()
